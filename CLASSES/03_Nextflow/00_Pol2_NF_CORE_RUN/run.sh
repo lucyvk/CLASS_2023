b2905382163f699250d7210fc5d93a1ce6bbd9fc
lucyvk <lucyvan19@gmail.com>
2023-02-17 13:20:49 -0700

    from R

--- CLASSES/03_Nextflow/00_Pol2_NF_CORE_RUN/run.sh
+++ CLASSES/03_Nextflow/00_Pol2_NF_CORE_RUN/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH -p short
-#SBATCH --job-name=YFTF_Chipseq
+#SBATCH -p long
+#SBATCH --job-name=MAX_JUN_test
 #SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=john.rinn@colorado.edu
+#SBATCH --mail-user=luva6824@colorado.edu
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --mem=6gb
-#SBATCH --time=11:00:00
+#SBATCH --time=14:00:00
 #SBATCH --output=nextflow.out
 #SBATCH --error=nextflow.erry
 pwd; hostname; date
@@ -20,7 +20,7 @@ nextflow run nf-core/chipseq -r 1.2.1 \
 --gtf /scratch/Shares/rinnclass/CLASS_2023/data/data/genomes/gencode.v32.annotation.gtf \
 --macs_gsize 3.2e9 \
 --blacklist /scratch/Shares/rinnclass/CLASS_2023/data/data/genomes/hg38-blacklist.v2.bed \
---email john.rinn@colorado.edu \
+--email luva6824@colorado.edu \
 -resume \
 -c nextflow.config
 date
